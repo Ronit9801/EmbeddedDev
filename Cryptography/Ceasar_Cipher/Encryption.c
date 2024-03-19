01fbdc127b607d63b7b1cd4cf10d133ea177fed6
Ronit0009 <ronitpatil0009@gmail.com>
2024-03-19 17:30:49 +0530

    Encrytion

---
+++ Cryptography/Ceasar_Cipher/Encryption.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    char ch[30];
+    short int shift, i=0, len;
+    printf("Enter the text to encrypt: ");
+    scanf("%[^\n]s",ch);
+    len = strlen(ch);
+    printf("Enter the count to shift: ");
+    scanf("%d", &shift);
+    do
+    {
+        if (ch[i] > 64 && ch[i] < 91)
+        {
+            ch[i]=(ch[i]+shift-65)%26+65;
+        }
+        if (ch[i] > 96 && ch[i] < 123)
+        {
+            ch[i]=(ch[i]+shift-97)%26+97;
+        }
+        i++;
+    } while (ch[i] != '\0');
+    printf("Encrypted text: ");
+    for (int j = 0; j < len; j++)
+    {
+        printf("%c", ch[j]);   
+    }
+    printf("\n");
+    return 0;
+}
